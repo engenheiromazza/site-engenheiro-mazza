ae0539e3f00aadf4bc920e02b8ae3f1ccb8fb486
engenheiromazza <william@wmazza.com>
2026-06-07 16:45:30 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -191,8 +191,9 @@ function Sobre() {
             position:"relative",
           }}>
             <img
-              src="/Foto_WMazza.png"
+              src="https://raw.githubusercontent.com/engenheiromazza/site-engenheiro-mazza/main/Foto_WMazza.png"
               alt="William Mazza"
+              onError={(e) => { e.target.style.display='none'; }}
               style={{ width:"100%", height:"100%", objectFit:"cover", objectPosition:"center top", display:"block" }}
             />
             {/* barra pêssego inferior */}
