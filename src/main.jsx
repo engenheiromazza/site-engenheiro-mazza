5ab6d15a7102f1ac1c6871c3c57f3808018c545b
engenheiromazza <william@wmazza.com>
2026-07-12 14:50:24 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import PaginaCursos from './Cursos.jsx'
+import { PaginaAvaliacao, PaginaDashboard } from './Avaliacoes.jsx'
 
 function Router() {
   const path = window.location.pathname;
-  if (path === '/cursos') return <PaginaCursos />;
+  if (path === '/cursos')    return <PaginaCursos />;
+  if (path === '/avaliar')   return <PaginaAvaliacao />;
+  if (path === '/avaliacoes') return <PaginaDashboard />;
   return <App />;
 }
 
